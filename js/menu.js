8fa42f4baba3ab1761f60f1e95a1cc92f3f247a7
javierruperez <javierruperez13@gmail.com>
2023-05-05 00:27:49 +0200

    Documentación del código

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,14 @@
+/*Variable constante que almacena el id "menu"*/
 const menu = document.getElementById("menu");
-
+/*Instrucción de interacción del menu hamburguesa, si clicas en las barras y no tiene asignada la propiedad "activo", se la asigna*/
 document.querySelector(".hamburguesa").addEventListener(
     "click", () => {
         menu.classList.toggle("activo")
     }
 )
+/*FIN DE Instrucción de interacción del menu hamburguesa, si clicas en las barras y no tiene asignada la propiedad "activo", se la asigna*/
+
+/*Instrucción de interacción del menu hamburguesa, si clicas en las barras y tiene asignada la propiedad "activo", se la retira*/
 document.querySelectorAll("#menu ul a").forEach(
     (enlace) => {
         enlace.addEventListener("click", () =>{
@@ -12,3 +16,4 @@ document.querySelectorAll("#menu ul a").forEach(
         })
     }
 )
+/*FIN DE Instrucción de interacción del menu hamburguesa, si clicas en las barras y tiene asignada la propiedad "activo", se la retira*/
