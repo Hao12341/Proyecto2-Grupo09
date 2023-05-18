94efab9bf69d759f918742929b59fb5c635a1be1
Heur-a <114075339+Heur-a@users.noreply.github.com>
2023-05-18 09:04:59 +0200

    Añadido cierre sesión, BBDD mejor documentada

--- html/Página Incidencias.php
+++ html/Página Incidencias.php
@@ -52,7 +52,7 @@ if(isset($_SESSION['user'])) {
           <li><a href="#Perfil">Mi perfil</a></li>
           <li><a href="#Sensores">Mis sensores</a></li>
           <li><a href="../html/contacto.html">Contáctanos</a></li>
-          <li><a href="#CerrarSesion">Cerrar sesión</a></li>
+          <li><a onclick="logout()">Cerrar sesión</a></li>
         </ul>
       </div>
     </div>
@@ -415,6 +415,7 @@ if(isset($_SESSION['user'])) {
   </div>
 
   </section>
+<script src="../js/cerrarSesion.js"></script>
 
 </body>
 >>>>>>> César:html/Página Incidencias.html
