2075f91d013d582de98b9ccb11bd6e08f0f1aab6
Heur-a <114075339+Heur-a@users.noreply.github.com>
2023-06-05 17:42:44 +0200

    Funciona otra vez php

--- src/api/includes/connexion.php
+++ src/api/includes/connexion.php
@@ -1,8 +1,8 @@
 <?php
-$bbdd_servidor = 'localhost';
-$bbdd_nombre = 'test_login';
-$bbdd_user = 'root';
-$bbdd_password = '';
+$bbdd_servidor = 'aesccar.upv.edu.es';
+$bbdd_nombre = 'aesccar_gti09';
+$bbdd_user = 'aesccar_user';
+$bbdd_password = 'SQLUser@2023';
 
 try {
 	$connexion = mysqli_connect($bbdd_servidor, $bbdd_user, $bbdd_password, $bbdd_nombre);
