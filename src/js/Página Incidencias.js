3fdc36c16dc1204286a5b18454c2934b6dcf2c4c
Shiza <ces.h5242@gmail.com>
2023-06-09 10:35:07 +0200

    Cambios Tecnicos

--- src/js/Página Incidencias.js
+++ src/js/Página Incidencias.js
@@ -1,4 +1,68 @@
 // Función para mostrar el pop-up
+function showPopUp1() {
+    // Obtiene el elemento del pop-up por su ID
+    var popUp = document.getElementById("popUpBox");
+    // Cambia la propiedad display a block para hacerlo visible
+    popUp.style.display = "block";
+
+}
+
+// Función para ocultar el pop-upz
+function hidePopUp1() {
+    // Obtiene el elemento del pop-up por su ID
+    var popUp = document.getElementById("popUpBox");
+    // Cambia la propiedad display a none para hacerlo invisible
+    popUp.style.display = "none";
+}
+
+function showPopUp() {
+    // Obtiene el elemento del pop-up por su ID
+    var popUp = document.getElementById("popUpBox");
+    // Cambia la propiedad display a block para hacerlo visible
+    popUp.style.display = "block";
+
+}
+
+// Función para ocultar el pop-upz
+function hidePopUp() {
+    // Obtiene el elemento del pop-up por su ID
+    var popUp = document.getElementById("popUpBox");
+    // Cambia la propiedad display a none para hacerlo invisible
+    popUp.style.display = "none";
+}
+
+function showPopUp() {
+    // Obtiene el elemento del pop-up por su ID
+    var popUp = document.getElementById("popUpBox");
+    // Cambia la propiedad display a block para hacerlo visible
+    popUp.style.display = "block";
+
+}
+
+// Función para ocultar el pop-upz
+function hidePopUp() {
+    // Obtiene el elemento del pop-up por su ID
+    var popUp = document.getElementById("popUpBox");
+    // Cambia la propiedad display a none para hacerlo invisible
+    popUp.style.display = "none";
+}
+
+function showPopUp() {
+    // Obtiene el elemento del pop-up por su ID
+    var popUp = document.getElementById("popUpBox");
+    // Cambia la propiedad display a block para hacerlo visible
+    popUp.style.display = "block";
+
+}
+
+// Función para ocultar el pop-upz
+function hidePopUp() {
+    // Obtiene el elemento del pop-up por su ID
+    var popUp = document.getElementById("popUpBox");
+    // Cambia la propiedad display a none para hacerlo invisible
+    popUp.style.display = "none";
+}
+
 function showPopUp() {
     // Obtiene el elemento del pop-up por su ID
     var popUp = document.getElementById("popUpBox");
