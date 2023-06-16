e6dc7500f4777de1e5a20e39c04269199b1bf2d4
javierruperez <javierruperez13@gmail.com>
2023-06-16 17:28:26 +0200

    Adios bbdd, hola bbdd

--- bbdd/EstadoIncidencia.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `EstadoIncidencia` (`idEstadoIncidencia`,`Valor Estado`) VALUES (1,'Pendiente');
-INSERT INTO `EstadoIncidencia` (`idEstadoIncidencia`,`Valor Estado`) VALUES (2,'En proceso');
-INSERT INTO `EstadoIncidencia` (`idEstadoIncidencia`,`Valor Estado`) VALUES (3,'Finalizado');
--- bbdd/unidades.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO `unidades` (`IdUnidades`,`Unidad`) VALUES (1,'g/L');
-INSERT INTO `unidades` (`IdUnidades`,`Unidad`) VALUES (2,'ph');
-INSERT INTO `unidades` (`IdUnidades`,`Unidad`) VALUES (3,'%');
-INSERT INTO `unidades` (`IdUnidades`,`Unidad`) VALUES (4,'Lúmenes');
-INSERT INTO `unidades` (`IdUnidades`,`Unidad`) VALUES (5,'ºC');
